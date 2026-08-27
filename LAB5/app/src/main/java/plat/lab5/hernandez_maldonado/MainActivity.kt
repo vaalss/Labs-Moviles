53a79290acb5fa73d2ae97d08217a5a3699a0be4
vaalss <valeria.hernandezmaldo@gmail.com>
2026-08-26 19:17:05 -0600

    arreglo de formato en cumpleaños

--- LAB5/app/src/main/java/plat/lab5/hernandez_maldonado/MainActivity.kt
+++ LAB5/app/src/main/java/plat/lab5/hernandez_maldonado/MainActivity.kt
@@ -8,14 +8,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import plat.lab5.hernandez_maldonado.ui.theme.LAB5Theme
 
@@ -35,27 +41,45 @@ fun View(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(16.dp)
     ) {
         Row(
-           modifier = Modifier.fillMaxWidth(),
+           modifier = Modifier
+               .fillMaxWidth()
+               .padding(4.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Column {
+            Column (
+                modifier = Modifier.weight(1f)
+            ) {
                 Text (
                     text = "Sábado",
-                    style = MaterialTheme.typography.headlineMedium
+                    style = MaterialTheme.typography.displaySmall,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
                 Text (
                     text = "12 de diciembre",
-                    style = MaterialTheme.typography.bodyLarge
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
                 )
             }
 
             OutlinedButton(
-                onClick = {}
+                modifier = Modifier
+                    .weight(1f)
+                    .offset(y = 20.dp),
+                onClick = {},
+                shape = RoundedCornerShape(6.dp),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = MaterialTheme.colorScheme.primary
+                )
             ) {
-                Text("Terminar jornada")
+                Text(
+                    text = "Terminar jornada",
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
         }
     }
