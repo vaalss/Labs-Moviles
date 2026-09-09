0f8bc94fb4fe2601daaa6168bfc774d0634f4f09
vaalss <valeria.hernandezmaldo@gmail.com>
2026-09-09 15:00:37 -0600

    Implementación inicial de historial

--- LAB6/app/src/main/java/plat/lab6/hernandez_maldonado/MainActivity.kt
+++ LAB6/app/src/main/java/plat/lab6/hernandez_maldonado/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
@@ -24,11 +27,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusTargetModifierNode
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -71,7 +74,7 @@ fun Contador(
         mutableIntStateOf(0)
     }
     val historial = remember {
-        mutableListOf<Movimientos>()
+        mutableStateListOf<Movimientos>()
     }
     val totalCambios = incrementos + decrementos
 
@@ -241,10 +244,19 @@ fun Contador(
                     fontWeight = FontWeight.Bold,
                     style = MaterialTheme.typography.headlineMedium
                 )
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(5)
+                ) {
+                    items (
+                        historial
+                    ) {
+                        Movimientos -> Text (
+                            text = "${Movimientos.valor}"
+                        )
+                    }
+                }
             }
-
-
-
         }
     }
 }
