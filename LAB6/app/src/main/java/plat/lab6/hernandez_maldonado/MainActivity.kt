a971a8db2478ed3485160c3b6c250fa8ed7a8265
vaalss <valeria.hernandezmaldo@gmail.com>
2026-09-09 18:36:08 -0600

    Implementación completa del historial

--- LAB6/app/src/main/java/plat/lab6/hernandez_maldonado/MainActivity.kt
+++ LAB6/app/src/main/java/plat/lab6/hernandez_maldonado/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,9 +14,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
@@ -32,6 +36,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -246,14 +251,33 @@ fun Contador(
                 )
 
                 LazyVerticalGrid(
-                    columns = GridCells.Fixed(5)
+                    columns = GridCells.Fixed(5),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    items (
-                        historial
-                    ) {
-                        Movimientos -> Text (
-                            text = "${Movimientos.valor}"
-                        )
+                    items (historial) { Movimientos ->
+                        val colorMovimientos = if (Movimientos.esIncremento) {
+                            Color.Green
+                        } else {
+                            Color.Red
+                        }
+
+                        Box (
+                            modifier = Modifier
+                                .height(45.dp)
+                                .background(
+                                    color = colorMovimientos,
+                                    shape = RoundedCornerShape(10.dp)
+                                ),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text (
+                                text = "${Movimientos.valor}",
+                                color = Color.White,
+                                style = MaterialTheme.typography.headlineSmall
+                            )
+                        }
+
                     }
                 }
             }
